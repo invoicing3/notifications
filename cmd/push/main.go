1763ffeafb5876fb93a02cc68e1f60f4ee19c9e5
ali abdalla <ali.dev.sd@gmail.com>
2022-10-18 12:34:21 +0200

    add command cli

---
+++ cmd/push/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "invoicing3-push-notification",
+	Short: "Invoicing3 push notification",
+	Long: `A Fast and Flexible Static Site Generator built with
+				  love by spf13 and friends in Go.
+				  Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+	  // Do Stuff Here
+	},
+  }
+  
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	  }
+}
